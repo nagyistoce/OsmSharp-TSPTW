c5718e3658bc1d9596d05d16bd7908fa2ab87ec2
xivk <ben@osmsharp.com>
2015-05-15 15:57:12 +0000

    First test using VNS.

--- OsmSharp.TSPTW.Benchmark/Program.cs
+++ OsmSharp.TSPTW.Benchmark/Program.cs
@@ -1,8 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// The MIT License (MIT)
+
+// Copyright (c) 2015 Ben Abelshausen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Reflection;
 
 namespace OsmSharp.TSPTW.Benchmark
 {
@@ -10,6 +29,24 @@ namespace OsmSharp.TSPTW.Benchmark
     {
         static void Main(string[] args)
         {
+            OsmSharp.Logging.Log.Enable();
+            OsmSharp.Logging.Log.RegisterListener(new OsmSharp.WinForms.UI.Logging.ConsoleTraceListener());
+
+            // set the seed manually.
+            OsmSharp.Math.Random.StaticRandomGenerator.Set(116542346);
+
+            var problem = OsmSharp.TSPTW.Parser.TSPTWProblemReader.Read(
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("OsmSharp.TSPTW.Benchmark.Problems.AFG.rbg010a.tw"));
+
+            var vnsSolver = new OsmSharp.Math.TSPTW.VNS.VNSSolver(
+                new OsmSharp.Math.TSPTW.Random.RandomSolver(),
+                new OsmSharp.Math.TSPTW.Random.Random1Shift(),
+                new OsmSharp.Math.TSPTW.LocalSearch.Local1Shift());
+            var fitness = 0.0;
+            var route = vnsSolver.Solve(problem, out fitness);
+
+            OsmSharp.Logging.Log.TraceEvent("Program", Logging.TraceEventType.Information, "Finished!");
+            Console.ReadLine();
         }
     }
 }
---
+++ OsmSharp.TSPTW/Parser/TSPTWProblemReader.cs
@@ -0,0 +1,82 @@
+﻿// The MIT License (MIT)
+
+// Copyright (c) 2015 Ben Abelshausen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using OsmSharp.Math.TSPTW;
+using OsmSharp.Math.VRP;
+using System;
+using System.IO;
+
+namespace OsmSharp.TSPTW.Parser
+{
+    /// <summary>
+    /// Reads TSPTW problems.
+    /// </summary>
+    public class TSPTWProblemReader
+    {
+        /// <summary>
+        /// Reads a TSP-TW problem from the given stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static IProblem Read(Stream stream)
+        {
+            var streamReader = new StreamReader(stream);
+
+            // first line has to contain problem size.
+            var line = streamReader.ReadLine();
+            var size = int.Parse(line);
+
+            // read weights.
+            var weights = new double[size][];
+            for(int x = 0; x < size; x++)
+            {
+                line = streamReader.ReadLine();
+
+                var lineWeights = new double[size];
+                var lineSplit = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                for(int y = 0; y < size; y++)
+                {
+                    lineWeights[y] = double.Parse(lineSplit[y], 
+                        System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture);
+                }
+
+                weights[x] = lineWeights;
+            }
+
+            // read timewindows.
+            var windows = new TimeWindow[size];
+            for (int x = 0; x < size; x++)
+            {
+                line = streamReader.ReadLine();
+
+                var lineSplit = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                windows[x] = new TimeWindow()
+                {
+                    Min = int.Parse(lineSplit[0]),
+                    Max = int.Parse(lineSplit[1])
+                };
+            }
+
+            return MatrixProblem.CreateATSPOpen(weights, windows);
+        }
+    }
+}
